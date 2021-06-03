30053269b22ba6aaa5799f18d0c9de712a49fdc9
Prajakta Gore <84570285+prajaktagore311@users.noreply.github.com>
2021-06-03 22:42:19 +0530

    Update Prajakta_001.java

--- src/in/amazon/testCases/Prajakta_001.java
+++ src/in/amazon/testCases/Prajakta_001.java
@@ -34,6 +34,16 @@ public class Prajakta_001 {
 	  Assert.assertEquals(SearchResults.getSearchResultText(browser), data.get(1));
   
   }
+	@Test
+  public void praju_001() throws Exception {
+	  browser = BrowserInit.getBrowser(TestObjects.browserName);
+	  browser.get(TestObjects.url);
+	  HomePage.enterSearchText(browser, data.get(3));
+	  HomePage.clickSearch(browser);
+	  Assert.assertEquals(SearchResults.getSearchResultText(browser), data.get(4));
+
+  }
+  
   @BeforeMethod
   public void beforeMethod() throws Exception {
 	  log = LogManager.getLogger("amazon");
